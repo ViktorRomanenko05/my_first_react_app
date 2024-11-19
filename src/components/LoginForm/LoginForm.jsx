9b2b243006042be666f6c36c68c22b99ac5beb7c
Viktor <1180505n@gmail.com>
2024-11-20 00:44:24 +0100

    homework04 fixed

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -4,14 +4,14 @@ import Button from "../Button/Button";
 
 function LoginForm  ()  {
     return (
-        <div className="login-form">
+        <form className="login-form">
             <h3 className="login-header">Login form</h3>
             <div className="inputs-container">
                 <Input type="text" placeholder="Enter your email" label="Email" name="email"/>
                 <Input type="password" placeholder="Enter your password" label="Password" name="password"/>
             </div>
             <Button type="submit" name="Login"></Button>
-        </div>
+        </form>
     )
 }
 
